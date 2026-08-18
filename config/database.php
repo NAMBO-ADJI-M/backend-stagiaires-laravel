908ca240984f0f76499cad1bc43a0cbba31cb3c0
unknown <adjimelinklaanambo@gmail.com>
2026-08-18 19:08:43 +0200

    jour

--- config/database.php
+++ config/database.php
@@ -60,8 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => file_exists(storage_path('certs/aiven-ca.pem'))
-                    ? storage_path('certs/aiven-ca.pem')
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')
+                    ? (Str::startsWith(env('MYSQL_ATTR_SSL_CA'), '/') ? env('MYSQL_ATTR_SSL_CA') : base_path(env('MYSQL_ATTR_SSL_CA')))
                     : null,
             ]) : [],
         ],
@@ -82,8 +82,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => file_exists(storage_path('certs/aiven-ca.pem'))
-                    ? storage_path('certs/aiven-ca.pem')
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')
+                    ? (Str::startsWith(env('MYSQL_ATTR_SSL_CA'), '/') ? env('MYSQL_ATTR_SSL_CA') : base_path(env('MYSQL_ATTR_SSL_CA')))
                     : null,
             ]) : [],
         ],
